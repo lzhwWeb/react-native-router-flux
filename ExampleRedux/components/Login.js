f467a6e6062958d7b4dda04eed36c483be0568c9
Hagen Hübel <hhuebel@itinance.com>
2016-05-29 11:47:53 +0200

    added increment and state to login page

--- ExampleRedux/components/Login.js
+++ ExampleRedux/components/Login.js
@@ -3,6 +3,10 @@ import {View, Text, StyleSheet} from "react-native";
 import Button from "react-native-button";
 import {Actions} from "react-native-router-flux";
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from './actions';
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -23,11 +27,17 @@ const styles = StyleSheet.create({
 });
 
 
-export default class extends React.Component {
+class Login extends React.Component {
     render(){
+		const { state, increment, decrement } = this.props;
+		console.log("Props 2: ", this.props, state, increment, decrement);
+
         return (
             <View style={[styles.container, this.props.style]}>
                 <Text>Login page: {this.props.data}</Text>
+				<Text>{state.count}</Text>
+
+				<Button onPress={increment}>Increment</Button>
                 <Button onPress={Actions.loginModal2}>Login 2</Button>
                 <Button onPress={() => Actions.refresh({title:"Changed title"})}>Change title</Button>
                 <Button onPress={Actions.pop}>Back</Button>
@@ -35,3 +45,11 @@ export default class extends React.Component {
         );
     }
 }
+
+export default connect(state => ({
+   state: state.counter
+ }),
+ (dispatch) => ({
+   actions: bindActionCreators(actions, dispatch)
+ })
+)(Login);
